77918ac039f2b223c029d079edabafd806887810
ragingo <save.the.world.crush@gmail.com>
2020-06-16 08:42:49 +0900

    [Bash] コーデック名の取得は、ffprobeで取得できた

--- Bash/ffmpeg/metadata/main.sh
+++ Bash/ffmpeg/metadata/main.sh
@@ -2,6 +2,19 @@
 
 set -eu
 
+ get_codec_name() {
+    local input_path=$1
+    ffprobe \
+        -v quiet \
+        -select_streams v:0 \
+        -show_entries stream=codec_name \
+        -of default=nokey=1:nw=1 \
+        -hide_banner \
+        "$input_path"
+}
+
+export -f get_codec_name
+
 # shellcheck disable=SC2010
 # shellcheck disable=SC2046
 dump() {
@@ -13,6 +26,21 @@ dump() {
         grep -E "^.+\.mp4$" | \
         xargs -P4 -n2 -I% ffmpeg -i % 2>&1 | \
         grep -E "Input|Stream" > dump.txt
+
+    popd
+ }
+
+# shellcheck disable=SC2010
+# shellcheck disable=SC2016
+# shellcheck disable=SC2046
+dump2() {
+    local input_dir=$1
+
+    pushd "$input_dir"
+
+    ls -1d $(find .) | \
+        grep -E "^.+\.mp4$" | \
+        xargs -P4 -n3 -I% bash -c 'name="$(get_codec_name %)"; echo "% $name"' 2>&1
 
     popd
  }
@@ -30,4 +58,6 @@ dump() {
 }
 
 # dump "$@"
-filter "$@"
+# filter "$@"
+
+dump2 "$@"
